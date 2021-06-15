9517bafd0edab48523ed1c43e90928cb58f586d1
Mito <76481138+mitomaamedia@users.noreply.github.com>
2021-06-15 12:41:52 +0300

    (main)

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Masymanwiki</title>
+  <html prefix="og: https://masymanwiki.org/#">
+  <html lang="en">
+  <meta charset="utf-8">
+  <meta property="og:url" content="https://masymanwiki.org" />
+  <meta name="DC.element" content="Value" />
+  <meta name="DCTERMS.element" content="Value" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Masymanwiki is free online (How to) guidebook. Powered by Maamedia. Made in Indonesia and Finland.">
+  <meta name="keywords" content="Maamedia, Masymanwiki, Maamedia Tools, toolskit, free guides, guidebook, free guidebook, free online tools, free online social network, how to">
+
+    <link rel="stylesheet" href="https://crm.maamedia.org/drafts/christmas/assets/css/global.css">
+<link rel='stylesheet' id='google-font-quicksand-montserrat-poppins-css' href='//fonts.googleapis.com/css?family=Quicksand%3A300%2C400%2C500%2C600%2C700%257CPoppins%3A400%2C400i%2C700%2C700i%257CMontserrat%3A400%2C500%2C600%2C700&#038;subset=latin%2Clatin-ext&#038;display=swap&#038;ver=5.5' media='all'/>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.createaccount {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.createaccount:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
+    h1 {
+        display: flex;
+        justify-content: center;
+        color: black;
+    }
+  
+    span::before {
+        content: "";
+        animation: animate infinite 5s;
+        padding-left: 10px;
+    }
+  
+    @keyframes animate {
+  
+        0% {
+            content: "wash a car";
+            color: green;
+        }
+  
+        50% {
+            content: "earn money";
+            color: red;
+        }
+  
+        75% {
+            content: "fell trees";
+            color: blue;
+        }
+    }
+}
+.hero           {
+  min-height: 800px;
+  text-align: center;
+  background-image: url('../img/sky.jpg');
+  background-size: cover;
+}
+.hero-content   {
+  color: #FFF;
+  padding-top: 130px;
+}
+.hero-content h1   {
+  font-size: 100px;
+  margin: 0;
+}
+.hero-content p    {
+  font-size: 50px;
+}
+.hero-content a    {
+  display: inline-block;
+  color: #FFF;
+  border: 3px solid #FFF;
+  border-radius: 3px;
+  padding: 15px 30px;
+  margin-right: 20px;
+  text-decoration: none;
+  font-size: 28px;
+}
+
+/* basic site sections */
+.site-section   {
+  padding-bottom: 50px;
+  padding-top: 50px;
+}
+.site-section-secondary   {
+  background: #EFEFEF;
+}
+.site-section-inside   {
+  width: 80%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.section-header {
+  text-align: center;
+  margin-bottom: 30px;
+}
+.section-header h2  {
+  font-size: 30px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  font-weight: 300;
+  color: #777;
+}
+
+/* feature boxes */
+.feature-box   {
+  display: inline-block;
+  width: 33%;
+  padding: 20px 30px;
+}
+.feature-box img  {
+  margin-bottom: 12px;
+  width: 80px;
+}
+.feature-box h5   {
+  font-size: 20px;
+  color: #999;
+  text-transform: uppercase;
+  font-weight: 300;
+  letter-spacing: 2px;
+  margin: 0;
+}
+
+/* testimonial boxes */
+.testimonial-box   {
+  display: inline-block;
+  width: 33%;
+  padding: 20px 30px;
+}
+.testimonial-content   {
+  background: #FFF;
+  padding: 15px;
+  border-radius: 4px;
+  margin-bottom: 15px;
+}
+.testimonial-author           {
+
+}
+.testimonial-author img {
+  float: left;
+  width: 80px;
+  border-radius: 50%;
+  margin-right: 15px;
+  vertical-align: middle;
+}
+.testimonial-author h5   {
+  font-size: 18px;
+  margin: 0;
+  padding-top:15px;
+}
+.testimonial-author span {
+  font-size: 14px;
+}
+
+</style>
+<script>
+
+</script>
+<ul>
+  <img src="https://commons.maamedia.org/images/b/b0/Masymanwiki_Logo.png" style="width:50px; height:50px; padding:8px; float:left;">
+  <li><a href="/">Home</a></li>
+  <li><a href="https://meta.maamedia.org/wiki/List_of_Masymanwikis">List of Masymanwikis</a></li>
+  <li><a href="/about">About</a></li>
+  <li style="float:right"><a href="https://en.masymanwiki.org/wiki/Special:CreateAccount">Create Account</a></li>
+</ul>
+
+<h1>How to <span> </span>?</h1><br>
+<p style="text-align: center">Find answer in Masymanwiki.</p>
+
+<div style="font-size: 250%; color: green; text-align: center;">What is Masymanwiki?</div>
+<div style="font-size:150%; text-align:center;"><b>Masymanwiki</b> is free online guidebook. It is Powered by <a href="https://maamedia.org">Maamedia</a> from Indonesia and Finland. How to... guidebook is created from quality instructions on how things work, how things are used, why someone exists. Masymanwiki strives to be a guidebook for everyone so that people learn to do things right and succeed. Masymanwiki is free to use, read, share, browse and edit. It is edited and maintained by volunteers in the same way as Maamedia. The information on Masymanwiki is reliable and the articles contain sources and references. This allows the reader to check the right of information if they wish. Masymanwiki is currently written in 4 different languages.<br><br>Masymanwiki is open to anyone living on Earth. <br>All people should have the right to use it in all ways at all times. System administrators are responsible for the availability, infrastructure, technology, maintenance and upgrades of Masymanwiki, and Masymanwiki is always ready to run smoothly and quickly.</div>
+<hr>
+
+<div class="site-section">
+    <div class="site-section-inside">
+        <div class="section-header">
+            <h2>Features</h2>
+        </div>
+
+        <div class="feature-box">
+            <img src="https://cdn3.iconfinder.com/data/icons/finance-business-10/32/Service-Care-Quick-Fast-512.png" alt="Quick to Use">
+            <h5>Quick to Use</h5>
+            <p>You don’t have to spend time to get an answer. The wiki is fast to use and its response time moves in seconds.</p>
+            </div>
+
+        <div class="feature-box">
+            <img src="https://pics.freeicons.io/uploads/icons/png/21126331841582994874-512.png" alt="Answer">
+            <h5>Answer to Everything</h5>
+            <p>When you browse and search, you are guaranteed to get the answer to everything.</p>
+            </div>
+
+        <div class="feature-box">
+            <img src="https://image.flaticon.com/icons/png/512/2238/2238026.png" alt="No Money - Free">
+            <h5>Free</h5>
+            <p>You do not need money or a credit card to access, share, read, or edit Masymanwiki (or its sister wikis). It is thus free for you!</p>
+            </div>
+            
+        <div class="feature-box">
+            <img src="https://icon-library.com/images/source-icon/source-icon-2.jpg" alt="References and Sources">
+            <h5>References and Sources</h5>
+            <p>Articles on Masymanwiki include sources and references. Thus, the reader can check the right of the information.</p>
+            </div>
+            
+        <div class="feature-box">
+            <img src="https://freeiconshop.com/wp-content/uploads/edd/code-flat.png" alt="Source code">
+            <h5>Source Code</h5>
+            <p>Masymanwiki and its sister wikis and the owner Maamedia is all open source on GitHub. Thus, anyone can see, edit and share the code and make it their own if they wish.</p>
+            </div>
+            
+        <div class="feature-box">
+            <img src="https://www.pngix.com/pngfile/middle/348-3484211_multi-language-icon-png-translation-service-transparent-png.png" alt="Multilingual">
+                
+            <h5>Multilingual</h5>
+            <p>Masymanwiki and Maamedia are both multilingual (that’s one of Maamedia’s goals). Masymanwiki, however, is written in only four languages. More language versions Maamedia is ready to do. Tools and databases that support Masymanwiki (Maamedia Data-Wiki) are also multilingual.</p>
+            </div>
+
+    </div>
+</div>
+<div class="section-header">
+    <h2>Masymanwikis</h2>
+</div>
+<div class="site-section">
+    <p style="text-align:center;">Masymanwiki is currently written in four different languages (Chinese, English, Finnish, and Indonesian). Maamedia has promised to create other Masymanwikes if there is a demand for them. A list of all language versions can be found on the <a href="https://meta.maamedia.org/wiki/List_of_Masymanwikis">Meta-Wiki</a>.</p>
+    <div style="text-align:center;">
+        <a href="https://en.masymanwiki.org"><button class="btn btn-primary">English Masymanwiki</button></a>
+        <a href="https://meta.maamedia.org/wiki/Masymanwiki"><button class="btn btn-primary">Masymanwiki on Meta</button></a>
+        
+        <a href="https://commons.maamedia.org/wiki/Category:Maamedia_Masymanwiki_logos"><button class="btn btn-primary">Masymanwiki logos</button></a>
+
+
+        
+    </div>
+</div>
+<style>
+footer {
+	position: fixed;
+	width: 100%;
+	left: 0;
+	bottom: 0;
+	background-color: brown;
+	color: white;
+	text-align: center;
+}
+.footerlink {
+    color: white;
+}
+</style>
+
+<footer>
+	<p>Copyright &copy; 2021 <a class="footerlink" href="https://maamedia.org">Maamedia</a>. | <a class="footerlink" href="https://maamedia.org/contact">Contact</a> | <a class="footerlink" href="https://maamedia.org/privacy-policy">Privacy Policy</a></p>
+</footer>
